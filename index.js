abd6f1e8bc5ed7dd95e338adf6f33728aae06221
David Vismans <david@vismans.net>
2023-05-29 15:09:01 +0200

    Added loop

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  console.log("Starting headless browser...");
-  const browser = await puppeteer.launch({
-    headless: "new",
-    executablePath: process.env.CHROMIUM_PATH,
-  });
-  const page = await browser.newPage();
-  await page.goto("https://www.google.com/");
+	while (true) {
+		console.log("Starting headless browser...");
+		const browser = await puppeteer.launch({
+			headless: "new",
+			executablePath: process.env.CHROMIUM_PATH,
+		});
+		const page = await browser.newPage();
+		await page.goto("https://www.google.com/");
 
-  await browser.close();
-  console.log("Worked!");
+		await browser.close();
+		console.log("Worked!");
+		// Pause the script for a definable number of minutes
+		const pauseDuration = 5; // Define the pause duration in minutes
+		const pauseMillis = pauseDuration * 60 * 1000; // Convert minutes to milliseconds
+		console.log(`Pausing for ${pauseDuration} minutes...`);
+		await new Promise(resolve => setTimeout(resolve, pauseMillis));
+		console.log(`Pausing ended...`);
+	}
 })();
